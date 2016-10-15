c2af4ab20b8c66c70c8aae1e48f6d05531c28890
Thomas Heinemann <data1701-e@web.de>
2016-10-15 14:29:47 +0200

    Separated generation controlling and CLI

--- src/main/java/local/jnitor/Jnitor.java
+++ src/main/java/local/jnitor/Jnitor.java
@@ -9,67 +9,19 @@ import java.io.Writer;
 import local.jnitor.exceptions.InitializationException;
 import local.jnitor.exceptions.JnitorException;
 import local.jnitor.model.Parameter;
-import local.jnitor.model.Type;
-import local.jnitor.wrappers.TypeWrapper;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateExceptionHandler;
 
 /**
- * Hello world!
+ * Main class for Jnitor program.
  *
+ * Contains the command line interface.
  */
 public class Jnitor 
 {
-	public Jnitor() throws InitializationException {
-    	cfg = new Configuration(Configuration.VERSION_2_3_23);
-
-		cfg.setClassLoaderForTemplateLoading(cfg.getClass().getClassLoader(), "/templates");
-    	
-    	cfg.setDefaultEncoding("UTF-8");
-    	
-    	cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-    	
-    	
-    	try {
-			this.header = cfg.getTemplate("template.h");
-		} catch (IOException e) {
-			throw new InitializationException(e);
-		}
-    	if (header == null)
-    	{
-    		throw new InitializationException("Template library did not return template object for header generation.");
-    	}
-    	
-    	try {
-			source = cfg.getTemplate("template.cpp");
-		} catch (IOException e) {
-			throw new InitializationException(e);
-		}
-    	if (source == null)
-    	{
-    		throw new InitializationException("Template library did not return template object for header generation.");
-    	}
-    	
-	}
-	
-	public void writeWrapperSources(Class<?> clazz, Writer headerWriter, Writer sourceWriter) throws JnitorException {
-		Type type = new Type(new TypeWrapper(clazz));
-    	
-    	try {
-    		header.process(type, headerWriter);
-			source.process(type, sourceWriter);
-		} catch (TemplateException | IOException e) {
-			throw new JnitorException(e);
-		}
-	}
-	
     public static void main( String[] args )
     {
-    	Jnitor jnitor;
+    	JnitorController jnitor;
     	try {
-			jnitor = new Jnitor();
+			jnitor = new JnitorController();
 		} catch (InitializationException e2) {
 			e2.printStackTrace();
 			return;
@@ -102,9 +54,4 @@ public class Jnitor
 		}
     	
     }
-    
-    private final Configuration cfg; 
-    
-    private final Template header;
-    private final Template source;
 }
