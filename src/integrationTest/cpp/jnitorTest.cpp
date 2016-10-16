a6b40ccfb7033f698c67a554f02c202455ffe5e7
Thomas Heinemann <data1701-e@web.de>
2016-10-17 00:13:03 +0200

    Added first test that actually executes generated code!

--- src/integrationTest/cpp/jnitorTest.cpp
+++ src/integrationTest/cpp/jnitorTest.cpp
@@ -2,11 +2,43 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include <cstdlib>
+
+#include <local.jnitor.ExampleClass.h>
+#include <jni.h>
+
+
+JavaVMOption options[1];
+JNIEnv *env;
+JavaVM *jvm;
+
 int main(int argc, char** argv) {
+	options[0].optionString = "-Djava.class.path=../../classes/main:../../classes/test";
+	JavaVMInitArgs vm_args;
+	memset(&vm_args, 0, sizeof(vm_args));
+	vm_args.nOptions = 1;
+	vm_args.options = options;
+	vm_args.version = JNI_VERSION_1_6;
+
+	long status = JNI_CreateJavaVM(&jvm, (void**)(&env), &vm_args);
+
+	if (status != JNI_OK) {
+		std::cerr << "Error initializing the JVM\n";
+		return -1;
+	}
+
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	int result = RUN_ALL_TESTS();
+	jvm->DestroyJavaVM();
+	return result;
 }
 
-TEST(jnitorTest, dummy) {
-	ASSERT_EQ(1, 1);
+TEST(jnitorTest, exampleClassTest) {
+	local::jnitor::ExampleClass exampleObject(env);
+
+
+	ASSERT_EQ(42, exampleObject.getMagicNumber());
+
+	exampleObject.setMagicNumber(23);
+	ASSERT_EQ(23, exampleObject.getMagicNumber());
 }
