bc2c6246428c49c9461fa83badd2f477dd843c27
Thomas Heinemann <data1701-e@web.de>
2016-10-19 20:20:08 +0200

    Added Javadoc to AbstractMethodWrapper.

--- src/main/java/com/github/theinemann/jnitor/wrappers/AbstractMethodWrapper.java
+++ src/main/java/com/github/theinemann/jnitor/wrappers/AbstractMethodWrapper.java
@@ -12,6 +12,15 @@ public abstract class AbstractMethodWrapper {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Creates the signature of a method, by analyzing the parameter and return types 
+	 * 
+	 * This signature can be used e.g. in an JNI callback to find the correct Java method.
+	 * 
+	 * @param parameters The parameters of the method
+	 * @param returnType The return type of the method
+	 * @return A string containing the JNI signature of the method.
+	 */
 	static protected String getSignatureFromTypes(Class<?>[] parameters, Class<?> returnType)
 	{
 		String result = "(";
