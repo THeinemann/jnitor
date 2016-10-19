cc072061879a8e55b75eb8a9833a5ff869d75243
Thomas Heinemann <data1701-e@web.de>
2016-10-19 21:09:29 +0200

    Added licensing information + "Generated code" warning to templates.

--- src/main/resources/templates/template.cpp
+++ src/main/resources/templates/template.cpp
@@ -1,4 +1,24 @@
 [#ftl]
+[#--
+Copyright 2016 Thomas Heinemann
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--]
+/* Warning: This file is generated.
+ * Do not change it unless you know exactly what you are doing.
+ * All your changes will be overwritten when this file is re-generated.
+ */
+
 #include "${headerFileName}"
 
 [#list packages as package]
